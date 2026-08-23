ee31a912ecedad1cdd810fccdfaf6076b1c76fcf
prakayushmsruas-hub <prakayush.msruas@gmail.com>
2026-08-23 17:56:30 +0530

    optimized palindromenumber

--- 015.Palindrome Number.py
+++ 015.Palindrome Number.py
@@ -1,16 +1,33 @@
+# def isPalindrome(x):
+#         if x < 0:
+#             return False
+
+#         original = x
+#         reverse = 0
+
+#         while x > 0:
+#             last_digit = x % 10
+#             reverse = reverse * 10 + last_digit
+#             x = x // 10
+
+#         return reverse == original  
+# print(isPalindrome(121))
+# print(isPalindrome(-121))
+# print(isPalindrome(1221))
+# print(isPalindrome(1321))
+
 def isPalindrome(x):
-        if x < 0:
-            return False
+    if x < 0 or (x % 10 == 0 and x != 0):
+        return False
 
-        original = x
-        reverse = 0
+    reverse_half = 0
 
-        while x > 0:
-            last_digit = x % 10
-            reverse = reverse * 10 + last_digit
-            x = x // 10
+    while x > reverse_half:
+        digit = x % 10
+        reverse_half = reverse_half * 10 + digit
+        x //= 10
 
-        return reverse == original  
+    return x == reverse_half or x == reverse_half // 10
 print(isPalindrome(121))
 print(isPalindrome(-121))
 print(isPalindrome(1221))
