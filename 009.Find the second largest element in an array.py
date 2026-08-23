5fb68466bc9889b78dfd20090931c00c864ac391
prakayushmsruas-hub <prakayush.msruas@gmail.com>
2026-08-23 17:59:00 +0530

    updated second largest and smallest

--- 009.Find the second largest element in an array.py
+++ 009.Find the second largest element in an array.py
@@ -3,39 +3,16 @@
 """You are given an array of integers.Your task is to find the second largest distinct element in the array
    without sorting the array"""
 
-try:
-    arr=list(map(int,input("Enter the elements in the array each seperated by a space:").split()))
-    if not arr:
+def second_largest(arr):
+    largest = arr[0]
+    second_largest = None
+    for num in arr:
+        if num > largest:
+            second_largest = largest
+            largest = num
+        elif num != largest and (second_largest == None or num > second_largest):
+            second_largest = num
+    return second_largest
 
-        print("Array is Empty!")
 
-    else:
-
-        largest=arr[0]
-        second_largest=float('-inf')
-
-        for index in range(len(arr)):
-
-            if arr[index]>largest:
-
-                second_largest=largest
-                largest=arr[index]
-                
-            elif arr[index]>second_largest and arr[index]!=largest:
-
-                second_largest=arr[index]     
-
-        if largest!=second_largest:
-
-            print(f"The largest element is {largest}") 
-
-            if second_largest!=float('-inf'): 
-
-                print(f"The second largest element is {second_largest}")  
-            else:
-
-                print("Second largest element is not in array")        
-               
-
-except ValueError:
-    print("Enter integers only!")
+print(second_largest([847, 213, 956, 421, 78, 634, 129, 502, 771, 345, 918, 267, 54, 689, 432, 105, 823, 376, 591, 244]))
--- 010.Find the Second Smallest Element in an Array.py
+++ 010.Find the Second Smallest Element in an Array.py
@@ -3,22 +3,15 @@
 """Write a Python program to find the second smallest distinct element in an array of integers.
 
    """
+def second_smallest(arr):
+    smallest = arr[0]
+    second_smallest = None
+    for num in arr:
+        if num < smallest:
+            second_smallest = num
+            smallest = num
+        elif num != smallest and (second_smallest == None or num < second_smallest):
+            second_smallest = num
+    return second_smallest
 
-arr = list(map(int, input("Enter the elements of array: ").split()))
-
-smallest = arr[0]
-second_smallest = float('inf')
-
-for i in range(len(arr)):
-    if arr[i] < smallest:
-        second_smallest = smallest
-        smallest = arr[i]
-
-    elif arr[i] < second_smallest and arr[i] != smallest:
-        second_smallest = arr[i]
-
-if second_smallest == float('inf'):
-    print("No second smallest element exists.")
-else:
-    print("Second smallest element:", second_smallest)
-
+print(second_smallest([847, 213, 956, 421, 78, 634, 129, 502, 771, 345, 918, 267, 54, 689, 432, 105, 823, 376, 591, 244]))
