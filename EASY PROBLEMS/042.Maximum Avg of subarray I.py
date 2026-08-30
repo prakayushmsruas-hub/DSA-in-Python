6b905e81278f923a2379af10c39bbd4cc16236e5
prakayushmsruas-hub <prakayush.msruas@gmail.com>
2026-08-30 21:26:22 +0530

    Maximum avg of subarray I

---
+++ EASY PROBLEMS/042.Maximum Avg of subarray I.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def findMaxAverage(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: float
+        """
+        window_sum=sum(nums[:k])
+        max_sum=window_sum
+
+        for right in range(k,len(nums)):
+            window_sum+=nums[right]
+            window_sum-=nums[right-k]
+
+            max_sum=max(max_sum,window_sum)
+        return float(max_sum)/k    
